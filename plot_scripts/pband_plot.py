774e83e45123399817ab27b18b1ece06a402c951
JYNi16 <49049752+JYNi16@users.noreply.github.com>
2023-07-19 13:45:15 +0800

    Add files via upload

--- plot_scripts/pband_plot.py
+++ plot_scripts/pband_plot.py
@@ -41,33 +41,113 @@ def read_band_data(file_path):
     return np.array(K[:2*num_kpoints]), E_bands, Orb
 
 def plot_band():
-    k_points, E_bands, weights = read_band_data("PBAND_SUM_SOC_Bi_Pz.dat")
+    k_points, E_bands, weights = read_band_data("PBAND_SUM_SOC_Te.dat")
     num_bands = E_bands.shape[1]
     
     #plot band structure 
     num_kpoints = int(len(k_points)/2)
     print("num_kpoints is:", num_kpoints)
-    fig = plt.figure(1, figsize=(10, 8))
+    fig = plt.figure(1, figsize=(10, 14))
+    k_sym_points = [0.0, 0.837, 1.804]
     
     #plot total bands
-    #for i in range(num_bands):
-    #    if i % 2 == 0:
-    #        plt.plot(k_points[:num_kpoints], E_bands[:,i], "red")
-    #    else:
-    #        plt.plot(k_points[num_kpoints:], E_bands[:,i], "red")
+    plt.subplot(221)
+    for i in range(num_bands):
+        if i % 2 == 0:
+            plt.plot(k_points[:num_kpoints], E_bands[:,i], "Red")
+        else:
+            plt.plot(k_points[num_kpoints:], E_bands[:,i], "Red")
+    plt.xlim(0, 0.527)
+    plt.ylim(-0.5, 0.5)
+    k_sym_label = ["M", r"$\Gamma$", "K"]
+    plt.ylabel("Energy(eV)")
+    plt.xlabel("Kpoints")
+    plt.xticks(k_sym_points, k_sym_label)
     
     #plot pbands 
+    plt.subplot(222)
     for j in range(num_bands):
         if j % 2 == 0:
-            plt.scatter(k_points[:num_kpoints], E_bands[:,j], c = weights[:,j])
+            plt.scatter(k_points[:num_kpoints], E_bands[:,j], c = weights[:,j], cmap="Blues")
         else:
-            plt.scatter(k_points[num_kpoints:], E_bands[:,j], c = weights[:,j])
+            plt.scatter(k_points[num_kpoints:], E_bands[:,j], c = weights[:,j], cmap="Blues")
     
-        
-    
+    plt.colorbar()  
+    plt.xlim(0,0.527)
     plt.ylim(-0.5, 0.5)
+    k_sym_label = ["M", r"$\Gamma$", "K"]
+    #plt.ylabel("Energy(eV)")
+    plt.xlabel("Kpoints")
+    plt.xticks(k_sym_points, k_sym_label)
+    plt.savefig("MBT_pband.png", dpi=300)
     plt.show()
 
+def plot_sub_band(file_name):
+    k_points, E_bands, weights = read_band_data(file_name)
+    num_bands = E_bands.shape[1]
+    
+    num_kpoints = int(len(k_points)/2)
+    print("num_kpoints is:", num_kpoints)
+    fig = plt.figure(1, figsize=(6, 7))
+    k_sym_points = [0.0, 0.837, 1.804]
+    
+    for j in range(num_bands):
+        if j % 2 == 0:
+            plt.scatter(k_points[:num_kpoints], E_bands[:,j], c = weights[:,j], cmap="Blues")
+        else:
+            plt.scatter(k_points[num_kpoints:], E_bands[:,j], c = weights[:,j], cmap="Blues")
+    
+    plt.colorbar()  
+    plt.xlim(0,1.804)
+    plt.ylim(-1, 1)
+    k_sym_label = ["M", r"$\Gamma$", "K"]
+    #plt.ylabel("Energy(eV)")
+    band_name = file_name.split(".")[0]
+    plt.title(band_name)
+    plt.xlabel("Kpoints")
+    plt.xticks(k_sym_points, k_sym_label)
+    plt.savefig(band_name+".png", dpi=300)
+    plt.show()
+    
+def plot_Bi_Te_band():
+    k_points, E_bands, weights = read_band_data("PBAND_SUM_SOC_Bi.dat")
+    k_points_Te, E_bands_Te, weights_Te = read_band_data("PBAND_SUM_SOC_Te.dat")
+    num_bands = E_bands.shape[1]
+    
+    #plot band structure 
+    num_kpoints = int(len(k_points)/2)
+    print("num_kpoints is:", num_kpoints)
+    fig = plt.figure(1, figsize=(6, 7))
+    k_sym_points = [0.0, 0.316, 0.527]
+    
+    #plot Bi pbands
+    for i in range(num_bands):
+        if i % 2 == 0:
+            plt.scatter(k_points[:num_kpoints], E_bands[:,i], c = weights[:,i], cmap="Reds")
+        else:
+            plt.scatter(k_points[num_kpoints:], E_bands[:,i], c = weights[:,i], cmap="Reds")
+    plt.colorbar() 
+    #plot Te pbands 
+    for j in range(num_bands):
+        if j % 2 == 0:
+            plt.scatter(k_points_Te[:num_kpoints], E_bands_Te[:,j], c = weights_Te[:,j], cmap="Blues")
+        else:
+            plt.scatter(k_points_Te[num_kpoints:], E_bands_Te[:,j], c = weights_Te[:,j], cmap="Blues")
+    
+    plt.colorbar()  
+    plt.xlim(0, 0.527)
+    plt.ylim(-0.5, 0.5)
+    k_sym_label = ["M", r"$\Gamma$", "K"]
+    #plt.ylabel("Energy(eV)")
+    plt.xlabel("Kpoints")
+    plt.xticks(k_sym_points, k_sym_label)
+    plt.savefig("MBT_pband_Bi_Te.png", dpi=300)
+    plt.show()
+
+    
 
 if __name__=="__main__":
-    plot_band()  
+    #file_name = ["PBAND_SUM_DW_Bi.dat", "PBAND_SUM_DW_Bi_Px.dat", "PBAND_SUM_DW_Bi_Py.dat", "PBAND_SUM_DW_Bi_Pz.dat"]
+    #for file in file_name:
+    #    plot_sub_band(file)  
+    plot_Bi_Te_band()
