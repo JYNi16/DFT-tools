0a3ea835d68dc64bef7083dd69c75195efb6a4d1
Curry <49049752+JYNi16@users.noreply.github.com>
2023-09-20 15:23:37 +0800

    Add files via upload

--- plot_scripts/band_plot_vasp_wannier.py
+++ plot_scripts/band_plot_vasp_wannier.py
@@ -29,19 +29,23 @@ def write_band_file(file_path):
     
     return k_v, E_v 
 
-def write_wannier_band():
+def write_wannier_band(file_name):
     num_bands = 1
+    Ef = 1.9997
     K_v, E_v = [], []
-    with open("wannier90_band.dat", "r") as f:
-        wv = f.readlines() 
+    with open(file_name, "r") as f:
+        wv = f.readlines()
     for i in range(len(wv)):
         if wv[i].strip() == "":
-            num_bands += 1
-            pass
+            if (i < len(wv) -2):
+                num_bands += 1
+                pass
+            else:
+                pass
         else:
             value = [float(s) for s in wv[i].split()]
             K_v.append(value[0])
-            E_v.append(value[1])
+            E_v.append(value[1] - Ef)
     
     k_points = int(len(K_v)/num_bands)
     E_v_w = np.zeros((k_points, num_bands))
@@ -51,36 +55,62 @@ def write_wannier_band():
     
     #print("num_bands is:", E_v_w.shape, "K_points is:", k_points)
     
-    return np.array(K_v[:274]), E_v_w, num_bands
+    return np.array(K_v[:k_points]), E_v_w, num_bands
             
 
 def plot_band():
-    k_v, E_v = write_band_file("REFORMATTED_BAND.dat")
-    num_ks, num_bands = E_v.shape[0], E_v.shape[1]
-    k_v_w, E_v_w, num_bands_w = write_wannier_band()
-    print(k_v_w.shape)
+    k_v_up, E_v_up = write_band_file("REFORMATTED_BAND_UP.dat")
+    k_v_dn, E_v_dn = write_band_file("REFORMATTED_BAND_DW.dat")
+    
+    num_ks, num_bands = E_v_up.shape[0], E_v_up.shape[1]
+    k_w_up, E_w_up, num_bands_w = write_wannier_band("wannier90.up_band.dat")
+    k_w_dn, E_w_dn, num_bands_w = write_wannier_band("wannier90.dn_band.dat")
+    
+    #print(k_w_up.shape)
+    #print(E_w_up.shape)
+    #print(num_bands_w)
     
     #plot the band structure calculated by DFT
-    fig = plt.figure(1,figsize=(10,8))
+    plt.figure(2, figsize=(12,6))
+    plt.subplot(121)
     for i in range(num_bands):
-        print(len(k_v), len(E_v[:,i]))
-        plt.plot(k_v, E_v[:,i], "black")
+         #print(len(k_v), len(E_v[:,i]))
+         plt.plot(k_v_up, E_v_up[:,i], "red")
+         #plt.plot(k_v_dn, E_v_dn[:,i], "black")
     
     #plot the band structure fitting by Wannier90
     for j in range(num_bands_w):
-        plt.plot(k_v_w, E_v_w[:,j], "red")
+        plt.plot(k_w_up, E_w_up[:,j], "black", linestyle="--")
+    #    plt.plot(k_w_dn, E_w_dn[:,j], "red", linestyle = "--")
     
-    plt.hlines(0,0,4)
-    
-    #plt.plot(k_v_w, E_v_w)
-    k_sym_points = [0.0, 0.505, 0.797, 1.380]
-    k_sym_label = ["G", "M", "K", "G"]
-    plt.ylim(-10, 10)
+    #k_sym_points = [0.0, 0.505, 0.797, 1.380]
+    #k_sym_label = ["G", "M", "K", "G"]
+    plt.ylim(-9, 6)
     #plt.xlim(0, 1.38)
     plt.ylabel("Energy(eV)")
     plt.xlabel("Kpoints")
     #plt.xticks(k_sym_points, k_sym_label)
-    plt.savefig("FeI3_soc.png", dpi=300)
+    #plt.savefig("band_up.png", dpi=300)
+    
+    plt.subplot(122)
+    for i in range(num_bands):
+         #print(len(k_v), len(E_v[:,i]))
+         plt.plot(k_v_dn, E_v_dn[:,i], "red")
+    
+    for j in range(num_bands_w):
+        plt.plot(k_w_dn, E_w_dn[:,j], "black", linestyle="--")
+    
+    #plt.hlines(0,0,4)
+    
+    #plt.plot(k_v_w, E_v_w)
+    #k_sym_points = [0.0, 0.505, 0.797, 1.380]
+    #k_sym_label = ["G", "M", "K", "G"]
+    plt.ylim(-9, 6)
+    #plt.xlim(0, 1.38)
+    plt.ylabel("Energy(eV)")
+    plt.xlabel("Kpoints")
+    #plt.xticks(k_sym_points, k_sym_label)
+    plt.savefig("band.png", dpi=300)
     plt.show()
     
     
