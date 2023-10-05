3459f0313ef2da575cacbc6ac19b4f2b088f150c
Curry <49049752+JYNi16@users.noreply.github.com>
2023-10-05 11:56:19 +0800

    Add files via upload

--- Fitting_scripts/fitting_rotation.py
+++ Fitting_scripts/fitting_rotation.py
@@ -26,7 +26,7 @@ def read_data():
         for i in range(len(data)): 
             value = [float(s) for s in data[i].split()]
             x.append(value[0]*p/18)
-            y.append(value[1])
+            y.append(value[1]*1000)
     f.close()
     
     const = y[0]
@@ -41,7 +41,7 @@ J = popt[0]
 B = popt[1]
 yvals = Second(x,J,B)
 print('Now the Fitting process only include J :')
-print('J is : {} meV'.format(round(J*1000, 6)))
+print('J is : {} meV'.format(round(J, 6)))
 print('constant: {}'.format(round(B, 6)))
 
 popt, pcov = curve_fit(Forth,x,y)
@@ -52,16 +52,20 @@ y_K = Forth(x,J_2,K_2,B_2)
 print("==========================================")
 print('Now the Fitting process include J and K :')
 #print('popt:' ,popt)
-print('J is : {} meV'.format(round(J_2*1000, 6)))
-print('K is : {} meV'.format(round(K_2*1000, 6)))
+print('J is : {} meV'.format(round(J_2, 6)))
+print('K is : {} meV'.format(round(K_2, 6)))
 print('constant: {}'.format(round(B_2, 6)))
 
-plot1 = plt.plot(x,y,'s',label='original values')
-plot2 = plt.plot(x,yvals,'r',label='only J values')
-plot2 = plt.plot(x,y_K,'b',label='add K values')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend(loc=4)
-plt.title('curve_fit')
+font = {'family': "Times New Roman", "weight":"normal", "size":20,}
+plt.figure(1, figsize=(10,8))
+plot1 = plt.scatter(x,y, s = 100, c="black", label='Tight Binding results')
+plot2 = plt.plot(x,yvals,'r',label=r'Fitting $A+JCos(\theta)$', linewidth=4)
+plot2 = plt.plot(x,y_K,'green',label=r'Fitting $A+JCos(\theta) + KCos^{2}(\theta)$', linewidth=4)
+plt.xlabel(r'Rotation angle($\theta$)', font)
+plt.ylabel('E($meV$)', font)
+plt.xticks(fontproperties = "Times New Roman", fontsize=20)
+plt.yticks(fontproperties = "Times New Roman", fontsize=20)
+plt.legend(loc=4, prop = {'family': "Times New Roman", "weight":"normal", "size":18,})
 plt.savefig("JK.png")
+#plt.ylim(-0.5, 70)
 plt.show()
